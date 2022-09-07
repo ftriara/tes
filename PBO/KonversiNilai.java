4bf9ef1f3d8fa5ab08dd72a3b042f4ff449bbb7d
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-07 09:11:57 +0700

    Update KonversiNilai.java

--- PBO/KonversiNilai.java
+++ PBO/KonversiNilai.java
@@ -5,7 +5,7 @@ public class KonversiNilai {
       Scanner scanner = new Scanner(System.in);
       System.out.print("Masukkan nilai anda    : ");
       int nilai = scanner.nextInt();
-      if (nilai >= 85 && nilai <= 100)
+      if (nilai >= 85)
         System.out.println("A");
       else if (nilai >= 80 && nilai < 85)
         System.out.println("A-");
@@ -19,9 +19,7 @@ public class KonversiNilai {
         System.out.println("C");
       else if (nilai >= 55 && nilai < 60)
         System.out.println("D");
-      else if (nilai <= 55)
+      else (nilai <= 55)
         System.out.println("E");
-      else
-        System.out.println("Indeks tidak tersedia.");
     }
 }
